debc814b2120fbdf994857e60d854d130077637b
Oleksandr Zolotarov <oleksandr.zolotarov@harmonicinc.com>
2021-09-24 18:23:23 +0300

    added logger

--- finmap_v2.py
+++ finmap_v2.py
@@ -7,9 +7,18 @@ import time
 import signal
 import sys
 import hashlib
+import logging
+from logging.handlers import RotatingFileHandler
 
+logging.basicConfig(filename='fn_ws.log',
+                    format='%(asctime)s - %(levelname)s:%(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    level=logging.INFO)
+                    
+logging.getLogger().addHandler(RotatingFileHandler(filename='fn_ws.log', maxBytes=100000000, backupCount=10))
+ 
 def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
+    logging.info('You pressed Ctrl+C!')
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -45,26 +54,46 @@ class Finmap:
         projects = []
         status = False
         try:
-            projects_response = requests.get(self.make_request_url(self.PROJECTS), headers=self.make_common_header())
+            request_url = self.make_request_url(self.PROJECTS)
+            headers = self.make_common_header()
+            
+            logging.debug('Finmap:get_projects - request_url: %s', request_url)
+            logging.debug('Finmap:get_projects - headers: %s', headers)
+            
+            projects_response = requests.get(url=request_url, headers=headers)
+            
+            logging.debug('Finmap:get_projects - response: %s', projects_response)
+            
             if (projects_response.status_code == 200):
                 status = True
                 jProjects = json.loads(projects_response._content.decode('utf8').replace("'", '"'))
                 for proj in jProjects:
                     projects.append(proj['label'])
         except Exception as e:
-            print('excetpion caught: ', str(e))
+            logging.error('Finmap:get_projects - excetpion caught: %s', str(e))
         return set(projects), status
         
     def make_finmap_proj(self, proj):
+        logging.info('Finmap:make_finmap_proj - creating: %s project', proj)
+        
         status = False
-        headers = self.make_common_header()
-        headers['Content-Type'] = 'application/json'
-        print('creating ', proj, ' project')
         try:
-            ret = requests.post(self.make_request_url(self.PROJECTS), headers=headers, data=json.dumps({'label': proj}))
-            status = ret.status_code == 201
+            request_url = self.make_request_url(self.PROJECTS)
+            headers = self.make_common_header()
+            headers['Content-Type'] = 'application/json'
+            data = json.dumps({'label': proj})
+            
+            logging.debug('Finmap:get_projects - request_url: %s', request_url)
+            logging.debug('Finmap:get_projects - headers: %s', headers)
+            logging.debug('Finmap:get_projects - data: %s', data)
+        
+            projects_response = requests.post(url=request_url, headers=headers, data=data)
+            
+            logging.debug('Finmap:make_finmap_proj - response: %s', projects_response)
+                        
+            status = projects_response.status_code == 201
         except Exception as e:
-            print('excetpion caught: ', str(e))
+            logging.error('Finmap:make_finmap_proj - excetpion caught: %s', str(e))
         return status
     
     
@@ -81,14 +110,14 @@ class DirCreator:
         proj_dir = self.make_proj_dir_path(proj_name)
         if os.path.exists(proj_dir):
             if not os.path.isdir(proj_dir):
-                print(proj_dir, ' already exists, but not a directory')
+                logging.error('DirCreator:make_project_dir - %s already exists, but not a directory', proj_dir)
         else:
-            print('creating ', proj_dir, 'dir')
+            logging.info('DirCreator:make_project_dir - creating %s dir', proj_dir)
             os.makedirs(proj_dir)
             if (os.path.exists(proj_dir)):
-                print('CREATED!!!')
+                logging.info('DirCreator:make_project_dir - %s dir CREATED!!!', proj_dir)
             else:
-                print('FAILED!!!')
+                logging.info('DirCreator:make_project_dir - %s dir creation FAILED!!!', proj_dir)
 
     def get_project_dirs(self):
         return set(os.listdir(self.PROJ_BASE_DIR) if os.path.exists(self.PROJ_BASE_DIR) else [])
@@ -118,8 +147,16 @@ class Worksection:
         projects = []
         status = False
         try:
-            projects_response = requests.get(self.make_request_url(self.GET_PROJECTS),
-                                             headers=self.make_common_header())
+            request_url = self.make_request_url(self.GET_PROJECTS)
+            headers = self.make_common_header()
+            
+            logging.debug('Worksection:get_projects - request_url: %s', request_url)
+            logging.debug('Worksection:get_projects - headers: %s', headers)
+            
+            projects_response = requests.get(url = request_url, headers=headers)
+            
+            logging.debug('Worksection:get_projects - projects_response: %s', projects_response)
+            
             if projects_response.status_code == 200:
                 jProjects = json.loads(projects_response._content.decode('utf8').replace("'", '"'))
                 if jProjects['status'] == 'ok':
@@ -127,18 +164,25 @@ class Worksection:
                     for proj in jProjects['data']:
                         projects.append(proj['name'])
         except Exception as e:
-            print('excetpion caught: ', str(e))
+            logging.error('Worksection:get_projects - excetpion caught: %s', str(e))
         return set(projects), status
         
     def make_worksection_proj(self, proj):
         status = False
         try:
-            projects_response = requests.get(self.make_request_url(self.POST_PROJECTS))
+            request_url = self.make_request_url(self.POST_PROJECTS)
+            
+            logging.debug('Worksection:make_worksection_proj - request_url: %s', request_url)
+            
+            projects_response = requests.get(url=request_url)
+
+            logging.debug('Worksection:make_worksection_proj - projects_response: %s', projects_response)
+
             if projects_response.status_code == 200:
                 jProjects = json.loads(projects_response._content.decode('utf8').replace("'", '"'))
                 status = jProjects['status'] == 'ok'
         except Exception as e:
-            print('excetpion caught: ', str(e))
+            logging.error('Worksection:make_worksection_proj - excetpion caught: %s', str(e))
         return status
     
 
@@ -155,8 +199,10 @@ def run():
         ws_projects, ws_status = worksection.get_projects()
         fm_projects, fn_status  = finmap.get_projects()
         dirs = dir_creator.get_project_dirs()
-        
-        #print(len(ws_projects))
+
+        logging.debug('run - ws_projects: %s', ws_projects)
+        logging.debug('run - fm_projects: %s', fm_projects)
+        logging.debug('run - dirs: %s', dirs)
         
         if len(ws_initial_set) == 0 and ws_status == True:
             ws_initial_set = ws_projects
@@ -167,28 +213,43 @@ def run():
         if len(dir_initial_set) == 0:
             dir_initial_set = dirs
             
+        logging.debug('run - fm_initial_set: %s', fm_initial_set)
+        logging.debug('run - ws_initial_set: %s', ws_initial_set)
+        logging.debug('run - dir_initial_set: %s', dir_initial_set)
+            
         initial_set = ws_initial_set.union(fm_initial_set).union(dir_initial_set)
         base_set = ws_projects.union(dirs).union(fm_projects)
         update_set = base_set - initial_set
         
+        logging.debug('run - initial_set: %s', initial_set)
+        logging.debug('run - base_set: %s', base_set)
+        logging.debug('run - update_set: %s', update_set)
+        
         dirs_to_create = update_set - dirs
         fm_to_create = update_set - fm_projects
         ws_to_create = update_set - ws_projects
+        
+        logging.debug('run - dirs_to_create: %s', dirs_to_create)
+        logging.debug('run - fm_to_create: %s', fm_to_create)
+        logging.debug('run - ws_to_create: %s', ws_to_create)
         
         for proj in dirs_to_create:
             dir_creator.make_project_dir(proj)
             
         for proj in fm_to_create:
             if finmap.make_finmap_proj(proj):
-                print('CREATED!!!')
+                logging.info('run - %s finmap project CREATED!!!', proj)
                 
 #        for proj in ws_to_create:
 #            if worksection.make_worksection_proj(proj):
-#                print('CREATED!!!')
+#                logging.info('run - %s worksection project CREATED!!!', proj)
+                
         
         time.sleep(1)
         
 if __name__ == '__main__':
+    logging.info('lights on')
     run()
+    logging.info('lights off')
 
 
